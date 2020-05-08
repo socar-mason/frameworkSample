407a6a5e6499c31d3021bd83dcc9c509ccbd636f
socar-mason <mason@socar.kr>
2020-05-08 15:26:57 +0900

    first commit

---
+++ MasonFramework/Greeting.swift
@@ -0,0 +1,15 @@
+//
+//  Greeting.swift
+//  MasonFramework
+//
+//  Created by Mason on 2020/05/08.
+//  Copyright © 2020 Mason. All rights reserved.
+//
+
+import Foundation
+
+open class Greeting {
+    public func greeting() {
+        print("Hello World!")
+    }
+}
---
+++ MasonFramework/MasonFramework.h
@@ -0,0 +1,19 @@
+//
+//  MasonFramework.h
+//  MasonFramework
+//
+//  Created by Mason on 2020/05/08.
+//  Copyright © 2020 Mason. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MasonFramework.
+FOUNDATION_EXPORT double MasonFrameworkVersionNumber;
+
+//! Project version string for MasonFramework.
+FOUNDATION_EXPORT const unsigned char MasonFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MasonFramework/PublicHeader.h>
+
+
